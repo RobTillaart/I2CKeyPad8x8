59fa5cb7334d60ea6e50a08f5fcd94a60a367743
Rob Tillaart <rob.tillaart@gmail.com>
2025-09-05 13:36:59 +0200

    fix

--- examples/I2Ckeypad8x8_demo01/I2Ckeypad8x8_demo01.ino
+++ examples/I2Ckeypad8x8_demo01/I2Ckeypad8x8_demo01.ino
@@ -26,8 +26,8 @@ void setup()
   Serial.begin(115200);
   Serial.println();
   Serial.println(__FILE__);
-  Serial.print("I2C_KEYPAD_LIB_VERSION: ");
-  Serial.println(I2C_KEYPAD_LIB_VERSION);
+  Serial.print("I2C_KEYPAD8x8_LIB_VERSION: ");
+  Serial.println(I2C_KEYPAD8x8_LIB_VERSION);
   Serial.println();
 
   Wire.begin();
